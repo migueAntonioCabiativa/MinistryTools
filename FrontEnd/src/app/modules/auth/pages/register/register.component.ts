8e7fa445410f6405111ab5880d1381fc87db366f
Anedvago <61481830+Anedvago@users.noreply.github.com>
2024-12-03 16:20:39 -0500

    creacion de la primera version de la pantalla de registro de usuario

--- FrontEnd/src/app/modules/auth/pages/register/register.component.ts
+++ FrontEnd/src/app/modules/auth/pages/register/register.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
-  imports: [],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  selector: "app-register",
+  imports: [FormsModule],
+  templateUrl: "./register.component.html",
+  styleUrl: "./register.component.css",
 })
 export class RegisterComponent {
+  user = {
+    name: "",
+    username: "",
+    password: "",
+  };
 
+  onSubmit() {
+    console.log("Usuario creado:", this.user);
+    alert("Usuario creado con éxito!");
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.user = {
+      name: "",
+      username: "",
+      password: "",
+    };
+  }
 }
